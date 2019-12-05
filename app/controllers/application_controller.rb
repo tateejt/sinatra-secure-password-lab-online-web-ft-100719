65248ecc5cd179df9fa00b791470efcf76573210
Emery Tate <tateejt@gmail.com>
2019-12-05 03:16:27 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,8 @@ class ApplicationController < Sinatra::Base
       redirect '/failure'
     else
       User.create(username: params[:username], password: params[:password])
+      redirect '/login'
+    end
 
   end
 
